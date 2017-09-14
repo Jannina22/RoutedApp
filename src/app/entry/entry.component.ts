9a704725f9ccf223ebaee70ccd0dc63c59cf9bd6
japps1a22 <japps1a22@cicct.local>
2017-09-14 12:13:14 +0800

    routed update

---
+++ src/app/entry/entry.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared.service';
+
+@Component({
+  selector: 'app-entry',
+  templateUrl: './entry.component.html',
+  styleUrls: ['./entry.component.css']
+})
+export class EntryComponent implements OnInit {
+
+  constructor(private dataStore: SharedService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(nameForm){
+    this.dataStore.addNewStudent(nameForm.value.number, 
+    nameForm.value.fname, 
+    nameForm.value.lname, 
+    nameForm.value.prog,
+    nameForm.value.yr)
+    nameForm.reset();
+  }
+
+}
